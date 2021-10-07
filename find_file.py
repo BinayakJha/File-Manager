c21cf637ba970efa92f51d384756f09acad73cc6
Binayak Jha <binayakjha@Binayak-MacBook.local>
2021-10-07 16:45:42 +0545

    Improvements in program

--- find_file.py
+++ find_file.py
@@ -1,6 +1,8 @@
 
 import os
 from rich.progress import track
+
+
 def find_folders(filename, search_path):
     return [
         os.path.join(root, filename)
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ except ImportError:
         """)
         os.system('pip install rich')
         os.system('pip install requests')
-        print("Packages installed :)")# console
+        print("Packages installed :)")  # console
         print("Checking If packages are installed or not")
         try:
             from inspect import trace
@@ -56,7 +56,8 @@ except ImportError:
             from rich.traceback import install
             from rich.markdown import Markdown
         except ImportError:
-            print("Sorry some error  occured while installing Packages \n Try installing manually")
+            print(
+                "Sorry some error  occured while installing Packages \n Try installing manually")
             print("Exiting")
             exit()
     os.system("cls" if os.name == "nt" else "clear")
@@ -71,15 +72,18 @@ MARKDOWN = """
 """
 md = Markdown(MARKDOWN)
 console.print(md)
+# formats of the files
+# videos and other common files
 image_formats = ["jpg", "jpeg", "png", "gif", "bmp"]
 video_formats = ["mp4", "mkv", "avi", "flv", "wmv", "mov"]
 audio_formats = ["mp3", "wav", "aac", "ogg", "flac", "mp4"]
 docs_formats = ["ai", "ait", "txt", "rtf"]
-
+# programming language files
 python_format = ["py", "pyc", "pyw", "pyo", "pyi"]
 jupyter_format = ["ipynb"]
 c_format = ["c", "cpp"]
 java_format = ["java"]
+# format of file end
 input = Text("Enter the folder name you want to manage: (Case sensitive) ")
 input.stylize("bold blue")
 console.print(input)
@@ -88,48 +92,78 @@ console.print(input)
 folder_input = console.input("=>")
 # finding files
 if os.name == "nt":
-    folder = find_file.find_folders(folder_input,"C:\\Users\\")[0]
+    folder = find_file.find_folders(folder_input, "C:\\Users\\")[0]
 else:
-    folder = find_file.find_folders(folder_input,"/Users/")[0]
+    folder = find_file.find_folders(folder_input, "/Users/")[0]
 print(folder)
 
 # functions
-def init():
-    if not os.path.isdir(os.path.join(folder,"python files")):
-        os.mkdir(os.path.join(folder,"python files"))
-    if not os.path.isdir(os.path.join(folder,"jupyter files")):
-        os.mkdir(os.path.join(folder,"jupyter files"))
-    if not os.path.isdir(os.path.join(folder,"c files")):
-        os.mkdir(os.path.join(folder,"c files"))
-    if not os.path.isdir(os.path.join(folder,"java files")):
-        os.mkdir(os.path.join(folder,"java files"))
-    
-init()
 while True:
     console.print("Started :thumbs_up:", style="bold green")
     sleep(3)
     files = os.listdir(folder)
     for file in track(files, description="Processing......"):
         ext = (file.split(".")[-1]).lower()
-        file11 = os.path.join(folder,file)
+        file11 = os.path.join(folder, file)
+
         if ext in image_formats:
-            shutil.move(file11, "images/"+file)
+            # making the folder only if it is not present
+            if not os.path.isdir(os.path.join(folder, "Images")) or not os.path.isdir(os.path.join(folder, "images")):
+                os.mkdir(os.path.join(folder, "Images"))
+
+            shutil.move(file11, folder+"/Images/"+file)
+
         elif ext in audio_formats:
-            shutil.move(file11, "audio/"+file)
+            # making the folder only if it is not present
+            if not os.path.isdir(os.path.join(folder, "Audio")):
+                os.mkdir(os.path.join(folder, "Audio"))
+
+            shutil.move(file11, folder+"/Audio/"+file)
+
         elif ext in video_formats:
-            shutil.move(file11, "videos/"+file)
+            # making the folder only if it is not present
+            if not os.path.isdir(os.path.join(folder, "Video")) or not os.path.isdir(os.path.join(folder, "video")) or not os.path.isdir(os.path.join(folder, "Videos")) or not os.path.isdir(os.path.join(folder, "videos")):
+                os.mkdir(os.path.join(folder, "Videos"))
+
+            shutil.move(file11, folder+"/Videos/"+file)
+
         elif ext in docs_formats:
-            shutil.move(file11, "docs/"+file)
-            console
+            # making the folder only if it is not present
+            if not os.path.isdir(os.path.join(folder, "Documents")):
+                os.mkdir(os.path.join(folder, "Documents"))
+
+            shutil.move(file11, folder+"/Documents/"+file)
+
         elif ext in python_format:
+            # making the folder only if it is not present
+            if not os.path.isdir(os.path.join(folder, "python files")):
+                os.mkdir(os.path.join(folder, "python files"))
+
             shutil.move(file11, folder+"/python files/"+file)
+
         elif ext in jupyter_format:
+            # making the folder only if it is not present
+            if not os.path.isdir(os.path.join(folder, "jupyter files")):
+                os.mkdir(os.path.join(folder, "jupyter files"))
+
             shutil.move(file11, folder+"/jupyter files/"+file)
+
         elif ext in c_format:
+            # making the folder only if it is not present
+            if not os.path.isdir(os.path.join(folder, "c files")):
+                os.mkdir(os.path.join(folder, "c files"))
+
             shutil.move(file11, folder+"/c files/"+file)
+
         elif ext in java_format:
+            # making the folder only if it is not present
+            if not os.path.isdir(os.path.join(folder, "java files")):
+                os.mkdir(os.path.join(folder, "java files"))
+
             shutil.move(file11, folder+"/java files/"+file)
+
         sleep(0.1)
     console.print("File moved :thumbs_up:", style="bold red")
+    console.print("Press Ctrl+c to end the program", style="bold blue")
     del files  # if program is using too much memory increae the value of sleep function
     sleep(5)
